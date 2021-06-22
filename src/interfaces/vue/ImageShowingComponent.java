237334302b97000ebfda9bb200921cea5d15e4a2
Florian CHACUN <florian.chacun@etu.univ-nantes.fr>
2021-06-22 13:34:12 +0000

    Update ImageShowingComponent.java

--- src/interfaces/vue/ImageShowingComponent.java
+++ src/interfaces/vue/ImageShowingComponent.java
@@ -26,7 +26,7 @@ public class ImageShowingComponent extends JComponent {
 
 
         try {
-            img = ImageIO.read(new File("./img/case_mer.jpg"));
+            img = ImageIO.read(new File("./img/case_normale.jpg"));
 
         } catch (IOException e) {
             e.printStackTrace();
