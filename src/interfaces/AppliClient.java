994c7c30c0307ebdcafd01daba35092f1385e8be
E205434Y <nathan.collinet@etu.univ-nantes.fr>
2021-06-22 10:07:41 +0200

    appli

--- src/interfaces/AppliClient.java
+++ src/interfaces/AppliClient.java
@@ -1,5 +1,6 @@
 package interfaces;
 
+import interfaces.vue.Jeu;
 import interfaces.vue.Menu;
 
 import javax.swing.*;
@@ -8,10 +9,14 @@ import java.awt.event.*;
 public class AppliClient {
     public static void main(String[] args) {
         Menu fenetre1 = new Menu("BattleShip BeeYard Edition");
+        Jeu fenetref = new Jeu("Partie de bataille navale - BeeYard en cours");
+
 
 
 
         fenetre1.pack();
         fenetre1.setVisible(true);
+        fenetref.pack();
+        fenetref.setVisible(true);
     }
 }
