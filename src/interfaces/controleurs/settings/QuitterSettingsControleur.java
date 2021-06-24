b009ccb50a329339847dea102b656b1d890e4390
E205434Y <nathan.collinet@etu.univ-nantes.fr>
2021-06-24 11:48:36 +0200

    quitter

--- src/interfaces/controleurs/settings/QuitterSettingsControleur.java
+++ src/interfaces/controleurs/settings/QuitterSettingsControleur.java
@@ -1,20 +1,37 @@
 package interfaces.controleurs.settings;
 
-import interfaces.vue.Settings;
+import interfaces.AppliClient;
+import interfaces.vue.*;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class QuitterSettingsControleur implements ActionListener {
 
     private Settings settings;
+    private PositionnementBateau pos;
+    private CreerOuRejoindre creerourej;
+    private ServeurChoix servchoix;
+    private Plateau plateau;
+    private MenuPrincipal menu;
 
-    public QuitterSettingsControleur(Settings settings){
+    public QuitterSettingsControleur(Settings settings, PositionnementBateau pos, CreerOuRejoindre creerourej, Plateau plateau, ServeurChoix servchoix, MenuPrincipal menu){
         this.settings = settings;
+        this.pos = pos;
+        this.plateau = plateau;
+        this.creerourej = creerourej;
+        this.menu = menu;
+        this.servchoix = servchoix;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("reviens au menu principal");
+        this.settings.dispose();
+        this.pos.dispose();
+        this.creerourej.dispose();
+        this.servchoix.dispose();
+        this.plateau.dispose();
+        this.menu.setVisible(true);
     }
 }
